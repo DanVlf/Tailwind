4c231efccbeecb6ff8ef0b27d40d44449ed61cf9
DanVlf <daniel.volf01@gmail.com>
2021-12-05 14:47:22 +0100

    master

---
+++ my-project/src/App.js
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Content from "./components/Content"
+
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Content />     
+    </>
+  );
+}
+
+export default App;
